9998be688a80f0466949cd3463e006773d0f5add
roshan1718 <roshan.shinde@agrevolution.in>
2020-12-30 10:19:04 +0530

    Modify: code to signUp page

--- src/component/signUp/signUp.jsx
+++ src/component/signUp/signUp.jsx
@@ -1,217 +1,189 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { Input} from 'antd';
+import { Link } from "react-router-dom";
+import { Input, Button, Form } from "antd";
 
-import '../signUp/signUp.scss';
-import user_service from '../../services/userService';
-
+import "../signUp/signUp.scss";
+import UserService from "../../services/userService";
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null,
+      confirmPassword: null,
+      formErrors: {
+        errorFirstName: "",
+        errorLastName: "",
+        errorEmail: "",
+        errorPassword: "",
+        errorConfirmPassword: "",
+      },
+    };
+  }
+  onValueChange = (e) => {
+    let nameValidation = /^[A-Z][a-zA-Z]*$/;
+    let emailValidation = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
 
-    constructor(props) {
-        super(props)
-        this.state = {
-
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-            confirmPassword: "",
-            error: {
-                errorFirstName: '',
-                errorLastName: '',
-                errorEmail: '',
-                errorPassword: '',
-                errorConfirmPassword: "",
-            }
-        }
-    }
-
-    onChangeFirstName = e => {
-        this.setState({
-            firstName: e.target.value
-        }, () => { console.log(this.firstName); })
-
-        let nameValidation = /^[A-Z]{1}[a-z]{2}[a-z]*$/;
-        let inputs = this.state.error;
-
-        console.log("Name validation", nameValidation.test(e.target.value));
-
-        inputs.errorFirstName = nameValidation.test(e.target.value) === true ? "" : "Name Start with caps and atleast 3 ";
-    }
-    onChangeLastName = e => {
-        this.setState({
-            lastName: e.target.value
-        }, () => { console.log(this.lastName); })
-
-        let nameValidation = /^[A-Z]{1}[a-z]{2}[a-z]*$/;
-        let inputs = this.state.error;
-
-        console.log("Last Name validation", nameValidation.test(e.target.value));
-
-        inputs.errorLastName = nameValidation.test(e.target.value) === true ? "" : "Name Start with caps and atleast 3 ";
-    }
-    onChangeEmail = e => {
-        this.setState({
-            email: e.target.value
-        }, () => { console.log(this.email); })
-
-        let emailValidation = /^([a-zA-Z0-9]*[.]*[a-zA-Z0-9]*@[a-zA-Z0-9]*.{1}[a-zA-Z0-9]*[.]*[a-zA-Z0-9]*)$/;
-        let inputs = this.state.error;
-
-        console.log("Email validation", emailValidation.test(e.target.value));
-
-        inputs.errorEmail = emailValidation.test(e.target.value) === true ? "" : "Enter Valid Email";
-    }
-    onChangePassword = e => {
-        this.setState({
-            password: e.target.value
-        }, () => { console.log(this.password); })
-
-        let passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$/;
-        let inputs = this.state.error;
-
-        console.log("Password validation", passwordValidation.test(e.target.value));
-
-        inputs.errorPassword = passwordValidation.test(e.target.value) === true ? "" :
-            "Enter Valid password";
-    }
-    onChangeConfirmPassword = e => {
-        this.setState({
-            confirmPassword: e.target.value
-        }, () => { console.log(this.confirmPassword); })
-
-        let passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$/;
-        let inputs = this.state.error;
-
-        console.log("Confirm Password validation", passwordValidation.test(e.target.value));
-
-        inputs.errorConfirmPassword = passwordValidation.test(e.target.value) === true ? ""
+    let inputs = this.state.formErrors;
+    switch (e.target.name) {
+      case "firstName":
+        inputs.errorFirstName =
+          nameValidation.test(e.target.value) === true
+            ? ""
+            : "Enter Valid first name";
+        break;
+      case "lastName":
+        inputs.errorLastName =
+          nameValidation.test(e.target.value) === true
+            ? ""
+            : "Enter Valid last name";
+        break;
+      case "email":
+        inputs.errorEmail =
+          emailValidation.test(e.target.value) === true
+            ? ""
+            : "Enter Valid email";
+        break;
+      case "password":
+        inputs.errorPassword =
+          passwordValidation.test(e.target.value) === true
+            ? ""
             : "Enter Valid password";
+        break;
+      case "confirmPassword":
+        inputs.errorConfirmPassword =
+          passwordValidation.test(e.target.value) === true
+            ? ""
+            : "Enter Valid password";
+        break;
+      default:
+        break;
     }
+  };
 
-    onSubmit = (event) =>{
-        event.preventDefault();
-        console.log("password", this.state.password);
-        console.log("confirm-password", this.state.confirmPassword);
+  onSubmit = (event) => {
+    event.preventDefault();
+    let userData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      password: this.state.password,
+      service: "advance",
+    };
+    UserService.register(userData).then((data) => {
+      console.log(data);
+    });
+  };
 
-        if(this.state.error.errorConfirmPassword ==="" 
-            && this.state.error.errorEmail ===""
-            && this.state.error.errorFirstName ===""
-            && this.state.error.errorLastName ===""
-            && this.state.error.errorPassword ==="")
-        {
-            if(this.state.password === this.state.confirmPassword){
-            let userData = {
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                email: this.state.email,
-                password: this.state.password,
-                service: 'advance',
-            };
-            user_service.register(userData).then((data) =>{
-                    console.log('data after register', data);
-            }).catch(error=>{
-                    console.log('Error', error);
-            })
-            }else {
-                alert("Password not Matching");
-            }
-        }
-        else {
-
-            alert("something is missing");
-        }
-    }
-
-    render() {
-        const { error } = this.state;
-        return (
-            <div className="register">
-                <div className="register-frame">
-                    <div className="reg-fundoo">
-                        <span className="f">F</span>
-                        <span className="u">u</span>
-                        <span className="n">n</span>
-                        <span className="d">d</span>
-                        <span className="o">o</span>
-                        <span className="u">o</span>
-                    </div>
-                    <div className="reg-input-body">
-                        <div className="input-info">
-                            <div className="reg-fundoo-account">
-                                Create your Fundoo Account
-                            </div>
-                            <form>
-                                <div className="reg-name">
-                                    <div className="f-name">
-                                        <Input
-                                            className="input-name"
-                                            label="First Name"
-                                            onChange={this.onChangeFirstName}
-                                            placeholder="First Name"/>
-                                        <p className="error-msg">{error.errorFirstName}</p>
-                                    </div>
-                                    <div className="f-name">
-                                        <Input
-                                            className="input-name"
-                                            label="Last Name"
-                                            onChange={this.onChangeLastName}
-                                            placeholder="Last Name"/>
-                                        <p className="error-msg">{error.errorLastName}</p>
-                                    </div>
-                                </div>
-                                <div className="reg-email-div">
-                                    <div className="f-name">
-                                        <Input
-                                            className="reg-email"
-                                            label="Email"
-                                            onChange={this.onChangeEmail}
-                                            placeholder="abc@example.com"/>
-                                        <p className="error-msg">{error.errorEmail}</p>
-                                    </div>
-                                </div>
-                                <span className="email-text">use letters, numbers & periods</span>
-                                <div className="reg-name reg-password">
-                                    <div className="f-name">
-                                        <Input
-                                            className="input-name"
-                                            label="Password"
-                                            type="password"
-                                            onChange={this.onChangePassword}
-                                            placeholder="Password"/>
-                                        <p className="error-msg">{error.errorPassword}</p>
-                                    </div>
-                                    <div className="f-name">
-                                        <Input
-                                            className="input-name"
-                                            label="Confirm Password"
-                                            onChange={this.onChangeConfirmPassword}
-                                            placeholder="Confirm Password"
-                                            type="password"/>
-                                        <p className="error-msg">{error.errorConfirmPassword}</p>
-                                    </div>
-                                </div>
-                                <span className="email-text">
-                                    Use 8 or more characters
-                                </span>
-                                <div className="direction-div">
-                                    <span className="signin">
-                                    <Link to="/">Sign in instead</Link>
-                                    </span>
-                                    <span className="input-group-btn">
-                                    </span>
-                                    <button
-                                        onClick={this.onSubmit}
-                                        className="next">
-                                        SignUp
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="outerCard">
+        <div className="fundooTextAlign">
+          <div className="Fcolor">F</div>
+          <div className="Fcolor">u</div>
+          <div className="Fcolor">n</div>
+          <div className="Fcolor">d</div>
+          <div className="Fcolor">o</div>
+          <div className="Fcolor">o</div>
+        </div>
+        <div className="font">
+          <h5>Create your Fundoo Account</h5>
+        </div>
+        <Form>
+          <div className="Passwordfield">
+            <div>
+              <Input
+                className="textSize"
+                id="outlined-basic"
+                placeholder="FirstName"
+                label="firstName"
+                name="firstName"
+                type="firstName"
+                required
+                onChange={this.onValueChange}
+              />
             </div>
-        )
-    }
+            <div>
+              <Input
+                className="textSize"
+                placeholder="LastName"
+                id="outlined-basic"
+                name="lastName"
+                type="LastName"
+                required
+                onChange={this.onValueChange}
+              />
+            </div>
+          </div>
+          <div className="Passwordfield">
+            <Input
+              className="emailFiled"
+              placeholder="Email"
+              id="outlined-basic"
+              label="E-mail"
+              name="email"
+              size="small"
+              required
+              onChange={this.onValueChange}
+            />
+          </div>
+
+          <div className="flex">
+            <span className="errorMessage">
+              {this.state.formErrors.errorEmail}
+            </span>
+          </div>
+
+          <div className="Passwordfield">
+            <div>
+              <Input
+                className="textSize"
+                id="outlined-basic"
+                label="Password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                onChange={this.onValueChange}
+              />
+            </div>
+            <div>
+              <Input
+                className="textSize"
+                placeholder="Confirm Password"
+                id="outlined-basic"
+                variant="outlined"
+                size="small"
+                name="confirmPassword"
+                type="password"
+                required
+                onChange={this.onValueChange}
+              />
+            </div>
+          </div>
+          <div className="flex">
+            <span className="errorMessage">
+              {this.state.formErrors.errorPassword}
+            </span>
+          </div>
+
+          <div className="buttons">
+            <Link to="/" className="links">
+              <h6>Sign in instead</h6>
+            </Link>
+            <Button className="loginbtn" type="submit" onClick={this.onSubmit}>
+              Create account
+            </Button>
+          </div>
+        </Form>
+      </div>
+    );
+  }
 }
